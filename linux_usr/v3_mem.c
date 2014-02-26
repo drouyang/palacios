499abffc8c5afa2a6ae65ad080a405adbb776c95
Brian Kocoloski <briankoco@cs.pitt.edu>
2014-02-25 19:32:20 -0500

    allow for full numa node offlining

--- linux_usr/v3_mem.c
+++ linux_usr/v3_mem.c
@@ -25,9 +25,11 @@ static void usage() {
 int main(int argc, char* argv[]) {
     int explicit = 0;
     int numa_zone = -1;
+    int num_blocks = 0;
 
     char * mem_str = NULL;
     int ret = 0;
+    struct v3_mem_region mem_range;
 
     /* Parse options */
     {
@@ -61,65 +63,75 @@ int main(int argc, char* argv[]) {
 	    return -1;
 	}
     }
+    
+    mem_str = argv[optind];
 
-    if (mem_str) {
-	struct v3_mem_region mem_range;
+    if (explicit) {
+	char * iter_str = NULL;
 
-	if (explicit) {
-	    char * iter_str = NULL;
+	while (iter_str = strsep(&mem_str, ",")) {
+	    int idx = atoi(iter_str);
 
-	    while (iter_str = strsep(&mem_str, ",")) {
-		int idx = atoi(iter_str);
-
-		if (pet_offline_block(idx) == -1) {
-		    printf("Error: Could not offline memory block %d\n", idx);
-		    continue;
-		}
-
-		mem_range.base_addr = idx * pet_block_size();
-		mem_range.num_pages = pet_block_size() / 4096;
-
-		if (pet_ioctl_path((char *)v3_dev, V3_ADD_MEMORY, &mem_range) != 0) {
-		    printf("Error: Could not add memory block %d to enclave\n", idx);
-		    continue;
-		}
+	    if (pet_offline_block(idx) == -1) {
+		printf("Error: Could not offline memory block %d\n", idx);
+		continue;
 	    }
 
-	} else {
-	    struct mem_block * block_arr = NULL;
-	    int cnt = atoi(mem_str);
-	    int i = 0;
-	    int ret = 0;
+	    mem_range.base_addr = idx * pet_block_size();
+	    mem_range.num_pages = pet_block_size() / 4096;
 
-	    block_arr = malloc(sizeof(struct mem_block) * cnt);
-	    memset(block_arr, 0, sizeof(struct mem_block) * cnt);
-
-	    ret = pet_offline_blocks(cnt, numa_zone, block_arr);
-
-	    if (ret != cnt) {
-		printf("Error: Could not allocate %d memory blocks\n", cnt);
-
-		pet_online_blocks(ret, block_arr);
-		free(block_arr);
-
-		return -1;
+	    if (pet_ioctl_path((char *)v3_dev, V3_ADD_MEMORY, &mem_range) != 0) {
+		printf("Error: Could not add memory block %d to enclave\n", idx);
+		continue;
 	    }
-
-	    for (i = 0; i < cnt; i++) {
-		mem_range.base_addr = block_arr[i].base_addr;
-		mem_range.num_pages     = block_arr[i].pages;
-
-		printf("Adding memory range (%p) to Palacios\n",
-		       (void *)mem_range.base_addr);
-
-		if (pet_ioctl_path((char *)v3_dev, V3_ADD_MEMORY, &mem_range) != 0) {
-		    printf("Error: Could not add memory block %d to enclave\n", block_arr[i].base_addr / pet_block_size());
-		    continue;
-		}
-	    }
-
-	    free(block_arr);
 	}
+
+    } else {
+	struct mem_block * block_arr = NULL;
+	int cnt = atoi(mem_str);
+	int i = 0;
+	int ret = 0;
+	int numa_num_blocks = pet_num_blocks(numa_zone);
+
+	if (strcmp(mem_str, "all") == 0) {
+	    cnt = numa_num_blocks;
+	} else {
+	    cnt = atoi(mem_str);
+	}
+
+	block_arr = malloc(sizeof(struct mem_block) * cnt);
+	memset(block_arr, 0, sizeof(struct mem_block) * cnt);
+
+	if (strcmp(mem_str, "all") == 0) {
+	    ret = pet_offline_node(numa_zone, block_arr);
+	    cnt = ret;
+	} else {
+	    ret = pet_offline_blocks(cnt, numa_zone, block_arr);
+	}
+
+	if (ret != cnt) {
+	    printf("Error: Could not allocate %d memory blocks\n", cnt);
+
+	    pet_online_blocks(ret, block_arr);
+	    free(block_arr);
+
+	    return -1;
+	}
+
+	for (i = 0; i < cnt; i++) {
+	    mem_range.base_addr = block_arr[i].base_addr;
+	    mem_range.num_pages     = block_arr[i].pages;
+
+	    printf("Adding memory range (%p) to Palacios\n",
+		   (void *)mem_range.base_addr);
+
+	    if (pet_ioctl_path((char *)v3_dev, V3_ADD_MEMORY, &mem_range) != 0) {
+		printf("Error: Could not add memory block %d to enclave\n", block_arr[i].base_addr / pet_block_size());
+		continue;
+	    }
+	}
+
+	free(block_arr);
     }
 
     return 0;
